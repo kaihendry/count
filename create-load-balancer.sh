d4b3498919feed8c9836c481cdf87d24aaa0c708
Kai Hendry <hendry@webconverger.com>
2017-01-21 20:43:05 +0800

    Update ELB creation script in light of pain in https://www.youtube.com/watch?v=5YF3qJM3uHAa

--- create-load-balancer.sh
+++ create-load-balancer.sh
@@ -1,21 +1,28 @@
 set -e
 
+# TODO Get this from ~/.ecs/config ??
 export CLUSTER_NAME=count
 
-subnet=$(aws ec2 describe-subnets | jq -r '.Subnets[].SubnetId')
-aws ec2 describe-security-groups | jq '.SecurityGroups[]| .Description + " " + .GroupId'
-
-select sggroup in $(aws ec2 describe-security-groups | jq -r '.SecurityGroups[].GroupId')
+echo Make sure this is the same as your ECS cluster!
+select VpcId in $(aws ec2 describe-security-groups | jq -r '.SecurityGroups[].VpcId' | grep -v null | uniq)
 do
 	break
 done
 
+echo Choose a security group for your ELB
+select sggroup in $(aws ec2 describe-security-groups | jq -r ".SecurityGroups[] | select(.VpcId==\"$VpcId\") | .GroupId")
+do
+	break
+done
+
+subnets=$(aws ec2 describe-subnets | jq -r ".Subnets[] | select(.VpcId==\"$VpcId\") | .SubnetId")
+
 echo sggroup $sggroup
-echo subnet $subnet
+echo subnet $subnets
 
 aws elb create-load-balancer --load-balancer-name "$CLUSTER_NAME" \
 	--listeners Protocol="HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80" \
-	--subnets $subnet --security-groups $sggroup
+	--subnets "$subnets" --security-groups $sggroup
 
 aws elb configure-health-check --load-balancer-name "$CLUSTER_NAME" \
 	--health-check Target=HTTP:80/,Interval=10,UnhealthyThreshold=2,HealthyThreshold=2,Timeout=3
