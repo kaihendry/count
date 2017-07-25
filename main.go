ca814537e329bbc9eb365252df31cc31f7b9b3d3
Kai Hendry <hendry@webconverger.com>
2017-07-25 17:00:46 +0800

    Lets use AWS ECR for my sanity

--- main.go
+++ main.go
@@ -117,7 +117,7 @@ func countpage(w http.ResponseWriter, r *http.Request) {
 <script src="static/main.js"></script>
 <title>{{ .Count }}</title>
 <style>
-body { background-color: green; }
+body { background-color: yellow; }
 </style>
 </head>
 <body>
