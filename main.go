08468322cbb4e2334ab1b78681202c03c93bd5ea
Kai Hendry <hendry@iki.fi>
2019-05-11 15:11:13 +0800

    8th refactor

--- main.go
+++ main.go
@@ -10,36 +10,32 @@ import (
 	"sync/atomic"
 )
 
-type viewCount int32
+type countHandler struct{ n int32 }
 
-var v viewCount
-
-func (n *viewCount) inc() (currentcount int32) {
-	return atomic.AddInt32((*int32)(n), 1)
-}
-
-func inc(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	fmt.Fprintf(w, "%d", v.inc()) // actual valid
+func main() { log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), routes())) }
+func (h *countHandler) inc() int32 {
+	return atomic.AddInt32(&h.n, 1)
 }
 
 func routes() *http.ServeMux {
+	h := countHandler{}
 	mux := http.NewServeMux()
-	mux.HandleFunc("/favicon.ico", http.NotFound)
 	fs := http.FileServer(http.Dir("./static"))
 	mux.Handle("/static/", http.StripPrefix("/static/", fs))
-	mux.HandleFunc("/metrics", prometheus)
-	mux.HandleFunc("/", countpage)
-	mux.HandleFunc("/inc/", inc)
+	mux.HandleFunc("/favicon.ico", http.NotFound)
+	mux.HandleFunc("/metrics", h.prometheus)
+	mux.HandleFunc("/", h.countpage)
+	mux.HandleFunc("/inc/", h.json)
 	return mux
 }
 
-func main() { log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), routes())) }
+func (h *countHandler) json(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, "%d", h.inc()) // actually valid JSON
+}
 
-func countpage(w http.ResponseWriter, r *http.Request) {
-
+func (h *countHandler) countpage(w http.ResponseWriter, r *http.Request) {
 	t := template.Must(template.New("index").ParseFiles("static/index.tmpl"))
-
 	envmap := make(map[string]string)
 	for _, e := range os.Environ() {
 		ep := strings.SplitN(e, "=", 2)
@@ -67,7 +63,7 @@ func countpage(w http.ResponseWriter, r *http.Request) {
 		Count  int32
 		Env    map[string]string
 		Header http.Header
-	}{v.inc(), envmap, r.Header})
+	}{h.inc(), envmap, r.Header})
 
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -75,6 +71,8 @@ func countpage(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func prometheus(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "# HELP count_total shows the in-memory count, which will get reset in the event of the lambda going cold or scaling.\n# TYPE count_total counter\ncount_total %d\n", v)
+func (h *countHandler) prometheus(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, `# HELP count_total shows the in-memory count, which will get reset in the event of the lambda going cold or scaling.
+# TYPE count_total counter
+count_total %d`, h.n)
 }
